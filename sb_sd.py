e94f5c9ce3095122fa5ad2f6011519f2abe68fad
Chaz Larson <chaz@chazlarson.com>
2021-11-21 14:20:19 -0600

    improvements

--- sb_sd.py
+++ sb_sd.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import uuid
+import argparse
 
 from apiclient import discovery
 from httplib2 import Http
@@ -15,26 +16,17 @@ from pathlib import Path
 # ##############################################################
 # You'll need the usual "client_secrets.json" file next to this
 # On first run you will be authenticated
-# Oh, and 'touch empty_file.bin' before running
 # ##############################################################
 
-# ##############################################################
-# WARNING: THERE IS NO CHECKING WHETHER THESE TEAMDRIVES EXIST
-# THE SCRIPT WILL HAPPILY CREATE DUPLICATES EVERY TIME YOU RUN IT
-# ##############################################################
-drivenames = [
-    'TEST-Movies',
-    'TEST-TV'
-]
+prefix = 'akIhSwlKdf'
 
-# ##############################################################
-# Creates MEDIA_DIR, then creates the others underneath that
-# ##############################################################
-MEDIA_DIR = 'Media'
-mediadirs = [
-    'Movies',
-    'TV',
-]
+group_email = "all-sa@chaztv.com"
+
+drive_data = {
+    'Movies':'/Media/Movies',
+    'Music':'/Media/Music',
+    'TV':'/Media/TV'
+}
 
 #     organizer = Manager
 #     fileOrganizer = Content manager
@@ -50,11 +42,12 @@ mediadirs = [
 # Really Actually Pretty Sorry about that.
 # ##############################################################
 user_emails_with_roles = {
-    'all-sa@chaztv.com': 'organizer'
+    group_email: 'organizer'
 }
 
 FOLDER_MIME = 'application/vnd.google-apps.folder'
 SOURCE_FILE = 'empty_file.bin'
+DRIVE_LOG = 'drive_create_log'
 BIN_MIME = "application/octet-stream"
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
@@ -62,7 +55,13 @@ store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
-    creds = tools.run_flow(flow, store)
+    parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[tools.argparser])
+    flags = parser.parse_args(['--noauth_local_webserver'])
+
+    creds = tools.run_flow(flow, store, flags)
 DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
 
 Path(SOURCE_FILE).touch()
@@ -83,12 +82,18 @@ def create_folder(root_id, folder):
     return DRIVE.files().create(body=body,
             supportsTeamDrives=True, fields='id').execute().get('id')
 
-def create_media_dirs(root_id):
-    f_id = create_folder(root_id, MEDIA_DIR)
-    print(f"** Folder {MEDIA_DIR} created, ID {f_id}")
-    for fn in mediadirs:
-        f1_id = create_folder(f_id, fn)
-        print(f"** Folder {fn} created, ID {f1_id}")
+def create_media_dirs(root_id, mediapath):
+    fld_id = root_id
+    pathList = mediapath.split('/')
+    for folder in pathList:
+        if len(folder) != 0:
+            q = "'" + fld_id + "' in parents and name='" + folder + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"
+            files = DRIVE.files().list(q=q).execute().get('files')
+            if not files:
+                fld_id = create_folder(fld_id, folder)
+                print(f"** Folder {folder} created, ID {fld_id}")
+            else:
+                fld_id = files[0].get('id')
 
 
 def create_bin_file_on_root(folder_id, fn, name):
@@ -96,19 +101,36 @@ def create_bin_file_on_root(folder_id, fn, name):
     return DRIVE.files().create(body=body, media_body=fn,
             supportsTeamDrives=True, fields='id').execute().get('id')
 
-for dn in drivenames:
-    td_id = create_td(dn)
-    print(f"** Team Drive {dn} created, ID: {td_id}")
-    for key in user_emails_with_roles:
-        role = user_emails_with_roles[key]
-        perm_id = add_user(td_id, key, role)
-        print(f"** user {key} created as {role}, ID: {perm_id}")
 
-    folder_name = f"-- {dn} Shared --"
-    folder_id = create_folder(td_id, folder_name)
-    print(f"** Folder {folder_name} created, ID {folder_id}")
-    mountfile = dn.lower().replace(' ', '_') + "_mounted.bin"
-    file_id = create_bin_file_on_root(td_id, SOURCE_FILE, mountfile)
-    print(f"** bin file created on root, ID {file_id}")
-    
-    create_media_dirs(td_id)
+for dn, mediapath in drive_data.items():
+    page_token = None
+    drivename = f"{prefix}-{dn}"
+    response = DRIVE.drives().list(
+            q=f"name contains '{drivename}'",
+            fields='nextPageToken, drives(id, name)',
+            useDomainAdminAccess = True,
+            pageToken=page_token).execute()
+    # if this drive doesn't exist
+    # then we can continue
+    if len(response.get('drives')) == 0:
+        td_id = create_td(drivename)
+        print(f"** Team Drive {drivename} created, ID: {td_id}")
+        with open(DRIVE_LOG, 'a') as f:
+            f.write(f"{drivename}|{td_id}\n")
+        for key in user_emails_with_roles:
+            role = user_emails_with_roles[key]
+            perm_id = add_user(td_id, key, role)
+            print(f"** user {key} created as {role}, ID: {perm_id}")
+
+        folder_name = f"-- {drivename} Shared --"
+        folder_id = create_folder(td_id, folder_name)
+        print(f"** Folder {folder_name} created, ID {folder_id}")
+        mountfile = drivename.lower().replace(' ', '_') + "_mounted.bin"
+        file_id = create_bin_file_on_root(td_id, SOURCE_FILE, mountfile)
+        print(f"** bin file created on root, ID {file_id}")
+        
+        create_media_dirs(td_id, mediapath)
+    else:
+        for drive in response.get('drives', []):
+            print(f"Found shared drive: {drive.get('name')} ({drive.get('id')})")
+
